2e779ccc85e4b6a99905e0543afd5c0edc2ab829
amro-kamal <37993690+amro-kamal@users.noreply.github.com>
2020-02-27 15:26:55 +0300

    added  user Auth

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/LogInActivity.java
@@ -0,0 +1,115 @@
+package org.tensorflow.lite.examples.classification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.tensorflow.lite.examples.classification.utils.MyPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogInActivity extends AppCompatActivity {
+    EditText username,userpassword;
+    ProgressBar login_pb;
+    Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        username=(EditText) findViewById(R.id.user_et);
+        userpassword=(EditText) findViewById(R.id.login_password_et);
+        loginBtn=(Button) findViewById(R.id.login_btn);
+        login_pb=(ProgressBar) findViewById(R.id.login_Pb);
+
+         RequestQueue requestqueue = Volley.newRequestQueue(this);
+
+        loginBtn.setOnClickListener(view -> {
+            //login here
+            login_pb.setVisibility(View.VISIBLE);
+            final String name=username.getText().toString().trim();
+            final String password=userpassword.getText().toString().trim();
+
+
+            final String url = NetworkingLab.END_POINT + "login";
+            Map<String, String> postParam= new HashMap<String, String>();
+            postParam.put("name", name);
+            postParam.put("password", password);
+            JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                    url, new JSONObject(postParam),
+                    new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                                    try {
+                                        String success=response.getString("message");
+                                        if(success.equals("success")){
+                                            Toast.makeText(LogInActivity.this,"Log in success",Toast.LENGTH_LONG).show();
+                                            boolean isFirstTime = MyPreferences.isFirstSelection(LogInActivity.this);
+                                            if (isFirstTime) {
+                                                Intent intent=new Intent(getApplicationContext(), modelSelectionActivity.class);
+                                                startActivity(intent);                    }
+                                            else{
+                                                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                                                startActivity(intent);
+                                            }
+
+                                        }
+                                        else if(success.equals("not registered")){
+                                            Toast.makeText(LogInActivity.this,"You don't have an account yet",Toast.LENGTH_LONG).show();
+                                            login_pb.setVisibility(View.GONE);
+
+                                        }
+
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                        login_pb.setVisibility(View.GONE);
+                                        Toast.makeText(LogInActivity.this,"Log in Error",Toast.LENGTH_LONG).show();
+
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            login_pb.setVisibility(View.GONE);
+                            Toast.makeText(LogInActivity.this,"Log in Error",Toast.LENGTH_LONG).show();
+
+
+                        }
+                    }) {
+                        @Override
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            HashMap<String, String> headers = new HashMap<String, String>();
+                            headers.put("Content-Type", "application/json; charset=utf-8");
+                            return headers;
+                        }
+
+                    };
+
+// Add the request to the RequestQueue.
+            requestqueue.add(jsonObjReq);
+        });
+
+
+    }
+    }
---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/RegisterActivity.java
@@ -0,0 +1,147 @@
+package org.tensorflow.lite.examples.classification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.tensorflow.lite.examples.classification.utils.MyPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText username,useremail,userpassword;
+    ProgressBar reg_pb;
+    Button regBtn;
+    RequestQueue requestqueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        username=(EditText) findViewById(R.id.user_name);
+        useremail=(EditText) findViewById(R.id.user_email);
+        userpassword=(EditText) findViewById(R.id.signup_password_et);
+        regBtn=(Button) findViewById(R.id.singup_btn);
+        reg_pb=(ProgressBar) findViewById(R.id.signup_Pb);
+         requestqueue = Volley.newRequestQueue(this);
+
+        regBtn.setOnClickListener(view -> {
+            register();
+        });
+
+
+}
+
+public void register(){
+//signup here
+reg_pb.setVisibility(View.VISIBLE);
+
+final String name=username.getText().toString().trim();
+final String email=useremail.getText().toString().trim();
+final String password=userpassword.getText().toString().trim();
+
+final String url = NetworkingLab.END_POINT + "register";
+
+        Map<String, String> postParam= new HashMap<String, String>();
+        postParam.put("name", name);
+        postParam.put("email", email);
+        postParam.put("password", password);
+
+    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+        url, new JSONObject(postParam),
+        new Response.Listener<JSONObject>() {
+
+@Override
+public void onResponse(JSONObject response) {
+        try {
+        String success=response.getString("message");
+        if(success.equals("success")){
+        Log.d("kkkk", "reg succes");
+
+        Toast.makeText(RegisterActivity.this,"Log in success",Toast.LENGTH_LONG).show();
+        boolean isFirstTime = MyPreferences.isFirstSelection(RegisterActivity.this);
+        if (isFirstTime) {
+        Intent intent=new Intent(getApplicationContext(), modelSelectionActivity.class);
+        startActivity(intent);                    }
+        else{
+        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        }
+
+        }
+        else if(success.equals("faild")){
+        Log.d("kkkk", "reg success");
+        Toast.makeText(RegisterActivity.this,"registeration faild",Toast.LENGTH_LONG).show();
+        reg_pb.setVisibility(View.GONE);
+        }
+
+
+        } catch (JSONException e) {
+        Log.d("kkkk", "reg error was catched");
+        e.printStackTrace();
+        reg_pb.setVisibility(View.GONE);
+        Toast.makeText(RegisterActivity.this,"Registeration Error",Toast.LENGTH_LONG).show();
+        }
+
+
+        }
+        }, new Response.ErrorListener()   {
+
+  @Override
+  public void onErrorResponse(VolleyError error) {
+        Log.d("kkkk", "reg error");
+        reg_pb.setVisibility(View.GONE);
+        Toast.makeText(RegisterActivity.this,"Log in Error",Toast.LENGTH_LONG).show();
+            }
+        }) {
+
+
+public Map<String, String> getHeaders() throws AuthFailureError {
+        HashMap<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json; charset=utf-8");
+        return headers;
+        }
+
+        };
+
+        requestqueue.add(jsonObjReq);
+           }
+
+
+}
+
+
+
+
+
+
+//@Override
+//                    protected Map<String, String> getParams() throws AuthFailureError {
+//                        Map<String, String> params = new HashMap<String, String>();
+//                        params.put("name", "amro");
+//                        params.put("password", "889656");
+//
+//
+//
+//                        return params;
+//                    }
