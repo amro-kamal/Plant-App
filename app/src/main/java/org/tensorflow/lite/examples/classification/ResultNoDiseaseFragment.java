8e53451a9062cd71f1a3db7b7f775d0abacb96e9
MaeenBadea <roronoamaeen@gmail.com>
2020-08-21 23:09:20 +0200

    create one fragment for no leaves and healthy

--- app/src/main/java/org/tensorflow/lite/examples/classification/ResultNoDiseaseFragment.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/ResultNoDiseaseFragment.java
@@ -1,11 +1,6 @@
 package org.tensorflow.lite.examples.classification;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,33 +16,37 @@ import com.bumptech.glide.request.RequestOptions;
 
 import org.tensorflow.lite.examples.classification.utils.Disease;
 
-import java.io.IOException;
 
-
-public class ResultHealthyFragment extends Fragment {
+public class ResultNoDiseaseFragment extends Fragment {
 
     private ImageView leafImg;
 
     private TextView diseaseIdentifiedTxt;
     private TextView confidenceTxtView;
+    private TextView commentTxt;
 
     private ImageView diseaseImg;
-    private ImageView healthyGif;
+    private ImageView gifImg;
 
     private Disease disease;
     private String leafImgUrl;
 
 
     private String confidence;
+    private String plantName;
+    
+    private boolean isHealthy;
 
-    public ResultHealthyFragment(Disease d ,String img , String conf) {
+    public ResultNoDiseaseFragment(Disease d , String img , String conf, String plantname, boolean h) {
         disease  = d;
         leafImgUrl = img;
         confidence = conf;
+        plantName = plantname;
+        isHealthy =h;
     }
 
-    public static ResultHealthyFragment newInstance(Disease disease , String leafImg , String confidence){
-        return new ResultHealthyFragment(disease ,leafImg , confidence);
+    public static ResultNoDiseaseFragment newInstance(Disease disease , String leafImg , String confidence, String plantName, boolean isHealthy){
+        return new ResultNoDiseaseFragment(disease ,leafImg , confidence, plantName, isHealthy);
     }
 
     @Nullable
@@ -55,25 +54,19 @@ public class ResultHealthyFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         super.onCreateView(inflater, container, savedInstanceState);
-        View v = inflater.inflate(R.layout.healthy_plant_result_frag, container ,false);
+        View v = inflater.inflate(R.layout.no_disease_plant_result_frag, container ,false);
         leafImg = v.findViewById(R.id.leafImg);
         diseaseImg = v.findViewById(R.id.diseaseImage);
         diseaseIdentifiedTxt = v.findViewById(R.id.diseaseIdentifiedTxt);
         confidenceTxtView= v.findViewById(R.id.model_confidence);
-        healthyGif= v.findViewById(R.id.healthyImg);
+        gifImg= v.findViewById(R.id.gifImg);
+        commentTxt= v.findViewById(R.id.commentTxt);
 
-
-
-
-
-
-        Log.i("kkkkk","inside healthhhhhhhhhhhhhhhhhhh");
         confidence = confidence.substring(0,6);
         float conf = 100 * Float.parseFloat(confidence);
-        String plant = ModelSingleton.getInstance(getActivity()).getCurrentModel();
-
-        diseaseIdentifiedTxt.setText("Your "+ plant +" Plant is healthy.");
+        diseaseIdentifiedTxt.setText("Your "+ plantName +" Plant is healthy.");
         confidenceTxtView.setText("confidence "+ conf+ "%");
+
 
         Glide.with(getActivity())  //2
                 .load(NetworkingLab.TEMP_URL+leafImgUrl) //3
@@ -98,9 +91,22 @@ public class ResultHealthyFragment extends Fragment {
                 .centerCrop() //4
                 .placeholder(R.drawable.placeholder) //5
                 .fallback(R.drawable.broken_image)
-                .into(healthyGif); //8
+                .into(gifImg); //8
 
 
+        if(!isHealthy){
+            //override all previous views
+            commentTxt.setText("No plant leaves detected.");
+            diseaseImg.setVisibility(View.INVISIBLE);
+            diseaseIdentifiedTxt.setVisibility(View.GONE);
+            confidenceTxtView.setVisibility(View.GONE);
+            Glide.with(getActivity())  //2
+                    .load("https://wf-live.enniscdn.net/wp-content/uploads/2019/05/20115802/Plant_V1.gif") //3
+                    .centerCrop() //4
+                    .placeholder(R.drawable.placeholder) //5
+                    .fallback(R.drawable.broken_image)
+                    .into(gifImg); //8
+        }
 
 
 
